d8a30ef2a4806b0466f5c2b846bdd09335acb9c9
tomthomas437@gmail.com <tomthomas437@gmail.com>
2021-11-27 01:53:18 +0530

    Insert 40 after 30 in LinkedList

---
+++ LinkedList.java
@@ -0,0 +1,19 @@
+package com.linkedList;
+
+public class LinkedList {
+	
+	Node head;
+	
+	public static void main(String[] args) {
+		
+		LinkedListServices list = new LinkedListServices(); 
+		list.addFirst(70);
+		list.addFirst(30);
+		list.addFirst(56);
+		list.printList();
+		
+		list.findNode(30);
+		list.insertBetween(30, 40);
+		list.printList();
+  }
+}
