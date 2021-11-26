2dc6703dca5f76b692a5538d4252ce105f64febf
tomthomas437@gmail.com <tomthomas437@gmail.com>
2021-11-27 01:42:58 +0530

    Adding 30 & 56 to 70

---
+++ LinkedList.java
@@ -0,0 +1,16 @@
+package com.linkedList;
+
+public class LinkedList {
+	
+	Node head;
+	
+	public static void main(String[] args) {
+		
+		LinkedListServices list = new LinkedListServices(); 
+		list.addFirst(70);
+		list.addFirst(30);
+		list.addFirst(56);
+		list.printList();
+		
+}
+}
