50dd6e9b69e5abab9229a0262b67b79823945be7
tomthomas437@gmail.com <tomthomas437@gmail.com>
2021-11-27 01:46:24 +0530

    Insert 30 between 56 & 70

---
+++ LinkedList.java
@@ -0,0 +1,18 @@
+package com.linkedList;
+
+public class LinkedList {
+	
+	Node head;
+	
+	public static void main(String[] args) {
+		
+		LinkedListServices list = new LinkedListServices(); 
+		list.addFirst(70);
+		list.addFirst(56);
+		list.printList();
+		
+		list.insertBetween(56, 30);
+		list.printList();
+		
+}
+}
